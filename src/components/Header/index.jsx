cad71474aab6eea15f7a1f4bcbdbbd74f50a8e24
Stanislav Efimtsev <evendot@yandex.ru>
2023-05-07 14:55:40 +0300

    Made the log out from an account

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,62 +7,85 @@ import {
   Container,
   IconButton,
   Typography,
-  Box
+  Box,
 } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 import { makeStyles } from "@material-ui/core/styles";
-import { useSelector } from "react-redux";
-import { selectIsAuth } from "../../redux/slices/auth";
-
+import { useSelector, useDispatch } from "react-redux";
+import { logout, selectIsAuth } from "../../redux/slices/auth";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   menuButton: {
-    marginRight: theme.spacing(1)
+    marginRight: theme.spacing(1),
   },
   title: {
-    flexGrow: 1
-  }
+    flexGrow: 1,
+  },
 }));
 
 export const Header = () => {
   const isAuth = useSelector(selectIsAuth);
+  const dispatch = useDispatch();
 
-  const onClickLogout = () => {};
+  const onClickLogout = () => {
+    if (window.confirm("Вы уверены, что хотите выйти?")) {
+      dispatch(logout())
+    }
+  };
+
   const classes = useStyles();
 
   return (
-      <AppBar position="relative" color="default">
-        <Container maxWidth="lg">
-            <Toolbar>
-              <IconButton edge="start" color="inherit" aria-label="menu" className={classes.menuButtom}>
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="h6" className={classes.title}>
-                <Link to="/" >Поворёнок</Link></Typography>
-              <Box m={10}>
-                  {isAuth ? (
-                    <>
-                      <Link to="/dishes/create">
-                        <Button color="primary" variant="contained">Добавить блюдо</Button>
-                      </Link>
-                        <Button color="secondary" variant="contained" onClick={onClickLogout}>Выйти</Button>
-                    </>
-                  ) : (
-                    <>
-                      <Link to="/login">
-                        <Button color="primary" variant="outlined">Войти</Button>
-                      </Link>
-                      <Link to="/register">
-                        <Button color="secondary" variant="contained">Зарегистрироваться</Button>
-                      </Link>
-                    </>
-                  )}
-              </Box>
-            </Toolbar>
-        </Container>
-      </AppBar>
+    <AppBar position="relative" color="default">
+      <Container maxWidth="lg">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            className={classes.menuButtom}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            <Link to="/">Поворёнок</Link>
+          </Typography>
+          <Box m={10}>
+            {isAuth ? (
+              <>
+                <Link to="/dishes/create">
+                  <Button color="primary" variant="contained">
+                    Добавить блюдо
+                  </Button>
+                </Link>
+                <Button
+                  color="secondary"
+                  variant="contained"
+                  onClick={onClickLogout}
+                >
+                  Выйти
+                </Button>
+              </>
+            ) : (
+              <>
+                <Link to="/login">
+                  <Button color="primary" variant="outlined">
+                    Войти
+                  </Button>
+                </Link>
+                <Link to="/register">
+                  <Button color="secondary" variant="contained">
+                    Зарегистрироваться
+                  </Button>
+                </Link>
+              </>
+            )}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 };
--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -1,46 +1,53 @@
-import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
-import axios from '../../axios'
-
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "../../axios";
 
 /**
  * @param params email и пароль
  */
-export const fetchAuth = createAsyncThunk('auth/fetchAuth', async (params) => {
-    const { data } = await axios.post('/auth/login', params);
-    return data;
-})
+export const fetchAuth = createAsyncThunk("auth/fetchAuth", async (params) => {
+  const { data } = await axios.post("/auth/login", params);
+  return data;
+});
 
 /**Начальное состояние для user */
 const initialState = {
-    data: null,
-    status: 'loading'
+  data: null,
+  status: "loading",
 };
 
 /** Slice для получения информации о user из async action.
  * После получения информация сохраняется в state.
  */
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    extraReducers: {
-        [fetchAuth.pending]: (state) => {
-            state.data = null;
-            state.status = 'loading';
-        },
-        /** При состоянии fullfilled - загружено */
-        [fetchAuth.fulfilled]: (state, action) => {
-            state.data = action.payload;
-            state.status = 'loaded';
-        },
-        /** Если при загрузке произошла ошибка */
-        [fetchAuth.rejected]: (state) => {
-            state.data = null;
-            state.status = 'error';
-        },
+  name: "auth",
+  initialState,
+  /**Выход из аккаунта */
+  reducers: {
+    logout: (state) => {
+      state.data = null;
     },
+  },
+  extraReducers: {
+    [fetchAuth.pending]: (state) => {
+      state.data = null;
+      state.status = "loading";
+    },
+    /** При состоянии fullfilled - загружено */
+    [fetchAuth.fulfilled]: (state, action) => {
+      state.data = action.payload;
+      state.status = "loaded";
+    },
+    /** Если при загрузке произошла ошибка */
+    [fetchAuth.rejected]: (state) => {
+      state.data = null;
+      state.status = "error";
+    },
+  },
 });
 
 /**Проверка, авторизован ли клиент */
-export const selectIsAuth = state => Boolean(state.auth.data);
+export const selectIsAuth = (state) => Boolean(state.auth.data);
 
 export const authReducer = authSlice.reducer;
+
+export const { logout } = authSlice.actions;
